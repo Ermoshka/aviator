3d8dd1742b22c23e7593b6a3eb91cbf86de567d2
brah517 <ekingsabyrzhanov@gmail.com>
2019-12-14 04:28:21 +0600

    added animations

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+let name = document.getElementById("name")
+let ava = document.getElementById("ava")
+let btn = document.getElementById("btn")
+let cert = document.getElementById("cert")
+
 let timeoutId = setTimeout(() => {
     preloader.style.display = 'none';
     maincontent.style.display = 'block';
+    ava.classList.add("show1");
+    name.classList.add("show2");
+    btn.classList.add("show3");
+    cert.classList.add("show4");
 }, 15 * 1000);
